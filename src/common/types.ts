53648ce9e389cc380871509495c832ebcd6924c2
Mariusz <siewruk.mariusz92@gmail.com>
2021-03-21 10:47:08 +0000

    Types tweaks

--- src/common/types.ts
+++ src/common/types.ts
@@ -3,7 +3,7 @@ import { Dispatch, SetStateAction } from 'react'
 export interface RouteElement {
   path: string
   id: string
-  component: React.FC
+  component: React.FC<any>
   exact?: boolean
 }
 
@@ -13,3 +13,8 @@ export declare type UseSessionStorage = [
   string,
   Dispatch<SetStateAction<string>>,
 ]
+
+export interface MatchParams {
+  username: string
+  postId: string
+}

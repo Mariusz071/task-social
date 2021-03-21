c1cfe1cc9bb696ba925ca46697510c1abfb75475
Mariusz <siewruk.mariusz92@gmail.com>
2021-03-21 08:26:22 +0000

    Added rendering posts one by one in the columns
    Post are being rendered one by one starting from the left column to the right.

--- src/screens/wallPage/WallPage.tsx
+++ src/screens/wallPage/WallPage.tsx
@@ -2,8 +2,12 @@ import * as React from 'react'
 
 import { Button } from 'components/button'
 import { InputField } from 'components/inputField'
+import { WallColumn } from 'components/wallColumn'
+
 import { history } from 'common/history'
 import { Context } from 'context'
+import { Post } from 'api'
+
 import './WallPage.scss'
 
 const clearUserData = (keys: string[]) => {
@@ -13,8 +17,32 @@ const clearUserData = (keys: string[]) => {
 }
 
 export const WallPage: React.FC = props => {
+  const [col1, setCol1] = React.useState<Post[]>([])
+  const [col2, setCol2] = React.useState<Post[]>([])
+  const [col3, setCol3] = React.useState<Post[]>([])
   const context = React.useContext(Context)
   const { posts } = context
+
+  React.useEffect(() => {
+    const lastPost = posts[posts.length - 1]
+
+    if (!lastPost) {
+      return
+    }
+
+    if (lastPost.id % 3 === 1) {
+      return setCol1([...col1, lastPost])
+    }
+
+    if (lastPost.id % 3 === 0) {
+      return setCol3([...col3, lastPost])
+    }
+
+    if (lastPost.id % 3 === 2) {
+      return setCol2([...col2, lastPost])
+    }
+  }, [posts])
+
   const onLogout = () => {
     clearUserData(['username', 'password'])
     history.push('/')
@@ -31,9 +59,9 @@ export const WallPage: React.FC = props => {
         />
       </nav>
       <main className='wall__main'>
-        <div>some posts</div>
-        <div>more posts</div>
-        <div>even more posts</div>
+        <WallColumn posts={col1} />
+        <WallColumn posts={col2} />
+        <WallColumn posts={col3} />
       </main>
     </div>
   )
